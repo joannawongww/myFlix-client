e6e0815aba90584525d3240f260619d943db1ee4
joannawongww <joanna_1994@hotmail.com>
2023-07-11 16:00:48 +0800

    useEffect()

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Silence of the Lamb",
-            description: "Young FBI",
-            genre: "Thriller",
-            director: "Jonathan Demme",
-            image: "silenceofthelambs.png"
-        },
-        {
-            id: 2,
-            title: "Avatar: The Way of Water",
-            description: "Blue-skinned humanoids seek refuse with aquatic clan of Pandora",
-            genre: "animated",
-            director: "James Cameron",
-            image: "avatar.png"
-        },
-        {
-            id: 3,
-            title: "Dumb and Dumber",
-            description: "Two dumb but well-meaning friends set out on cross-country trip to return briefcase full of money to its owner",
-            genre: "Comedy",
-            director: "Peter Farrelly",
-            image: "dumbanddumber.png"
-            }
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("#")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.docs.map( (doc) => {
+            return {
+                id: doc.key,
+                title: doc.title,
+                image: "#",
+                author: doc.author_name?.[0]
+            };
+        });
+        setMovies(moviesFromApi);
+
+    });
+    }, [])
 
     if (selectedMovie) {
         return <MovieView movie={selectedMovie}
